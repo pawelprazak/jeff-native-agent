0730564e045fd6fb6d3f520dedca89b92ead01cd
Paweł Prażak <pawel.prazak@bluecatcode.com>
2016-02-21 14:59:35 +0100

    Remove boost.hpp

--- src/boost.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef JEFF_NATIVE_AGENT_BOOST_HPP
-#define JEFF_NATIVE_AGENT_BOOST_HPP
-
-#include <boost/optional.hpp>
-#include <boost/assert.hpp>
-#include <boost/smart_ptr.hpp>
-#include <boost/format.hpp>
-
-#endif //JEFF_NATIVE_AGENT_BOOST_HPP
